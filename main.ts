58bc9356c5cc153f4cbe4ca4e1cf6594a80dcf03
Reluzapro <98704219+Reluzapro@users.noreply.github.com>
2022-02-03 07:56:49 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+let a2 = 0
+let x2 = 0
+let y2 = 0
+input.onButtonPressed(Button.A, function () {
+    while (a2 == 0) {
+        x2 = 0
+        y2 = 0
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, x2)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, y2)
+        if (input.buttonIsPressed(Button.A)) {
+            a2 = 1
+        }
+    }
+})
